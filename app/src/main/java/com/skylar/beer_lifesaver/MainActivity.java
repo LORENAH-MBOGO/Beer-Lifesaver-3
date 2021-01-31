cb56bfa9dd104d85b59c952dab674067ed3de4c4
Lorenah <lorayne911@gmail.com>
2021-01-31 19:58:14 +0300

    successfully added splash screen!

--- app/src/main/java/com/skylar/beer_lifesaver/MainActivity.java
+++ app/src/main/java/com/skylar/beer_lifesaver/MainActivity.java
@@ -10,6 +10,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+import static java.lang.Thread.sleep;
+
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.loginButton)
     Button loginButton;
@@ -23,6 +25,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        startActivity(new Intent(MainActivity.this, SplashActivity.class));
+        try {
+            sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // close splash activity
+        finish();
+
 
         aboutButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,23 +41,23 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, AboutActivity.class);
                 startActivity(intent);
 
-            }
-        });
+
+                loginButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                        startActivity(intent);
 
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        signUpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
-                startActivity(intent);
+                        signUpButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                                startActivity(intent);
+                            }
+                        });
+                    }
+                });
             }
         });
     }
--- app/src/main/java/com/skylar/beer_lifesaver/SplashActivity.java
+++ app/src/main/java/com/skylar/beer_lifesaver/SplashActivity.java
@@ -2,27 +2,16 @@ package com.skylar.beer_lifesaver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SplashActivity extends AppCompatActivity {
-
-    Handler handler;
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-
-        handler=new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        },3000);
-
+        setContentView(R.layout.activity_main);
     }
 }
