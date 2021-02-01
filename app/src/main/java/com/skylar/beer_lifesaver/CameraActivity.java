080c0e77545aff175024bd38f03cbf97f0601dc8
Lorenah <lorayne911@gmail.com>
2021-02-01 07:45:40 +0300

    set request code method

--- app/src/main/java/com/skylar/beer_lifesaver/CameraActivity.java
+++ app/src/main/java/com/skylar/beer_lifesaver/CameraActivity.java
@@ -2,11 +2,16 @@ package com.skylar.beer_lifesaver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class CameraActivity extends AppCompatActivity {
 
@@ -40,7 +45,29 @@ public class CameraActivity extends AppCompatActivity {
                 uploadImage();
             }
         });
+    }
+        private void chooseImage() {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
 
-
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null )
+        {
+            filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                imageView.setImageBitmap(bitmap);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
 }
