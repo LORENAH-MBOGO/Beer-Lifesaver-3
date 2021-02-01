248f0085151afe544c723dd3f40fc7aa6e132868
Lorenah <lorayne911@gmail.com>
2021-02-01 07:42:50 +0300

    create instance variable.

--- app/src/main/java/com/skylar/beer_lifesaver/CameraActivity.java
+++ app/src/main/java/com/skylar/beer_lifesaver/CameraActivity.java
@@ -2,9 +2,19 @@ package com.skylar.beer_lifesaver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class CameraActivity extends AppCompatActivity {
+
+    private Button btnChoose, btnUpload;
+    private ImageView imageView;
+
+    private Uri filePath;
+
+    private final int PICK_IMAGE_REQUEST = 71;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
